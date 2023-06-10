959269ce6936a33402b6bfcc6f8b2c62095f38f8
akaics <gulsumerd38@gmail.com>
2023-06-10 21:26:56 +0200

    Add project files.

---
+++ Badetidssystemet2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Badetidssystemet2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
